7f66a21cfc03332cb291151ea844873ecce11485
Carlos Renato <64719682+car-lo-s@users.noreply.github.com>
2024-11-26 21:47:50 -0300

    atualização

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,10 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo {get;  set;}
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string Imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = Imei;
+            Memoria = memoria;
+
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var a = new Nokia("123","tinjolo","123456789",64);
+var b = new Iphone("456","iphone 25","789",32);
+
+
+a.Ligar();
+a.ReceberLigacao();
+a.InstalarAplicativo("wpp");
+
+b.Ligar();
+b.ReceberLigacao();
+b.InstalarAplicativo("joguitos");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
